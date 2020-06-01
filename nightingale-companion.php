40a89d79f84f5597d0cf53c93dd6d4fd40fe1c66
Nick Summerfield <nicksummerfield@macbook-pro.lan>
2020-06-01 16:58:52 +0100

    Recreating Master

--- nightingale-companion.php
+++ nightingale-companion.php
@@ -6,7 +6,7 @@
 * Author: Nick Summerfield, NHS Leadership Academy
 * License: GPL v3
 * Requires at least: 5.0
-* Tested up to: 5.3.2
+* Tested up to: 5.4
 * Version: 1.0
 *
 * @package nightingale-companion
