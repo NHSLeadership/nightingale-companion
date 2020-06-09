95f0fa3c366d122465cb28089575f84f4de05684
Tony Blacker <tblacker7@gmail.com>
2020-06-09 09:02:15 +0100

    corrected typo

--- performance/performance-enhancements.php
+++ performance/performance-enhancements.php
@@ -2,31 +2,32 @@
 /**
  * Performance tweaks to improve load times and overall performance of site.
  *
- * @package nightingale-companion
+ * @package   nightingale-companion
  * @copyright NHS Leadership Academy, Nick Summerfield
- * @version 1.0 2nd June 2020
+ * @version   1.0 2nd June 2020
  */
 // load css stuff.
-if ( ( isset( $nightingale_companion_options['loadcss'] ) ) && ( $nightingale_companion_options['loadcss'] === 'on' ) ) {
+if ( ( isset( $nightingale_companion_options[ 'loadcss' ] ) ) && ( $nightingale_companion_options[ 'loadcss' ] === 'on' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'loadcss.php' );
 }
 
 // instantpage stuff.
-if ( ( isset( $nightingale_companion_options['instantpage'] ) ) && ( $nightingale_companion_options['instantpage'] === 'on' ) ) {
+if ( ( isset( $nightingale_companion_options[ 'instantpage' ] ) ) && ( $nightingale_companion_options[ 'instantpage' ] === 'on' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'instantpage.php' );
 }
 
 // defer JS loading to footer.
-if ( ( isset( $nightingale_companion_options['defer_js'] ) ) && ( $nightingale_companion_options['defer_js'] === 'on' ) ) {
+if ( ( isset( $nightingale_companion_options[ 'defer_js' ] ) ) && ( $nightingale_companion_options[ 'defer_js' ] === 'on' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'defer-js.php' );
 }
 
 // set cache headers.
-if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options['browser_cache'] ) ) && ( $nightingale_companion_options['browser_cache'] > 0 ) ) {
-require_once( plugin_dir_path( __FILE__ ) . 'cache-headers.php' );
+if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options[ 'browser_cache' ] ) ) && ( $nightingale_companion_options[ 'browser_cache' ] > 0 ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'cache-headers.php' );
+}
 
 //lazy loading - this will eventually be part of core WP
-if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options['lazyloading'] ) ) && ( $nightingale_companion_options['lazyloading'] === 'on' ) ) {
+if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options[ 'lazyloading' ] ) ) && ( $nightingale_companion_options[ 'lazyloading' ] === 'on' ) ) {
 	if (
 		! function_exists( 'wp_lazy_loading_enabled' ) &&
 		! function_exists( 'wp_filter_content_tags' ) &&
@@ -38,21 +39,21 @@ if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options['lazyloading'] 
 }
 
 // disableemojis.
-if ( ( isset( $nightingale_companion_options['disable_emojis'] ) ) && ( $nightingale_companion_options['disable_emojis'] === 'on' ) ) {
+if ( ( isset( $nightingale_companion_options[ 'disable_emojis' ] ) ) && ( $nightingale_companion_options[ 'disable_emojis' ] === 'on' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'disable-emojis.php' );
 }
 
 // cleanup WP headers.
-if ( ( isset( $nightingale_companion_options['cleanup_meta'] ) ) && ( $nightingale_companion_options['cleanup_meta'] === 'on' ) ) {
+if ( ( isset( $nightingale_companion_options[ 'cleanup_meta' ] ) ) && ( $nightingale_companion_options[ 'cleanup_meta' ] === 'on' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'clean-wp-headers.php' );
 }
 
-if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options['compress_html'] ) ) && ( $nightingale_companion_options['compress_html'] === 'on' ) ) {
+if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options[ 'compress_html' ] ) ) && ( $nightingale_companion_options[ 'compress_html' ] === 'on' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'basic-minify.php' );
 }
 
-if  ( ( !is_admin() ) && ( isset( $nightingale_companion_options['move_scripts'] ) )&& ($nightingale_companion_options['move_scripts
-'] === 'on' ) ) {
-    require_once( plugin_dir_path( __FILE__ ) . 'script-to-footer.php' );
+if ( ( ! is_admin() ) && ( isset( $nightingale_companion_options[ 'move_scripts' ] ) ) && ( $nightingale_companion_options[ 'move_scripts
+' ] === 'on' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'script-to-footer.php' );
 }
 
